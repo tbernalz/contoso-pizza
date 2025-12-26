5c1202917c6b3a21624d4fb6b0f78add730b3176
Tomás Bernal <tbernalz@eafit.edu.co>
2025-12-26 18:10:57 +0100

    feat(pizza): add get one pizza endpoint

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -12,4 +12,15 @@ public class PizzaController : ControllerBase
 
     [HttpGet]
     public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
 }
