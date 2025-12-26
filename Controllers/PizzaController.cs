53de53dda7c70fbc132346bdcbfdd7f7665ca7a9
Tomás Bernal <tbernalz@eafit.edu.co>
2025-12-27 00:13:49 +0100

    feat(pizza): add POST endpoint

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -23,4 +23,11 @@ public class PizzaController : ControllerBase
 
         return pizza;
     }
+
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
 }
